64a8892ebcf173a58725e41e1a966178c525ce6f
Davor Kolarević <Davor Kolarevi>
2016-08-31 22:48:43 +0200

    Fix rating calculator bug
    In calculate function problem was with new rating counter which was
    incremented from rating variable instead of old rating counter variable.

--- app/ratingCalculator.js
+++ app/ratingCalculator.js
@@ -6,7 +6,7 @@
 *	An+1 = An * n/(n+1) + an+1/(n+1)
 */
 module.exports.calculateRating = function(oldRating, oldRatingCount, rating) {
-	var newRatingCount = oldRating + 1;
+	var newRatingCount = oldRatingCount + 1;
 
 	var newRating = oldRating * oldRatingCount/newRatingCount + rating/newRatingCount;
 
---
+++ test/ratingCalculatorTest.js
@@ -0,0 +1,46 @@
+//ratingCalculatorTest.js
+
+var assert = require('assert');
+var ratingCalculator = require('../app/ratingCalculator');
+
+var old_rating_1 = 4;
+var old_rating_count_1 = 1;
+var new_rating_1 = 2;
+
+var old_rating_2 = 5;
+var old_rating_count_2 = 1;
+var new_rating_2 = 1;
+
+var old_rating_3 = Math.round((5 + 4 + 4 + 3 + 4 + 5 + 3 + 5 + 5 + 2 + 3 + 5 + 5 + 5)/14);
+var old_rating_count_3 = 14;
+var new_rating_3 = 2;
+
+var old_rating_4 = Math.round((1 + 2 + 2 + 3 + 1 + 2)/6);
+var old_rating_count_4 = 6;
+var new_rating_4 = 3;
+
+describe('ratingCalculator', function() {
+	describe('#ratingCalculator(rating_1)', function() {
+		it('should return valid result', function() {
+			assert.equal(3, ratingCalculator.calculateRating(old_rating_1, old_rating_count_1, new_rating_1));
+		});
+	});
+
+	describe('#ratingCalculator(rating_2)', function() {
+		it('#should return valid result', function() {
+			assert.equal(3, ratingCalculator.calculateRating(old_rating_2, old_rating_count_2, new_rating_2));
+		});
+	});
+
+	describe('#ratingCalculator(rating_3)', function() {
+		it('should return valid result', function() {
+			assert.equal(4, ratingCalculator.calculateRating(old_rating_3, old_rating_count_3, new_rating_3));
+		});
+	});
+
+	describe('#ratingCalculator(rating_4)', function() {
+		it('should return valid result', function() {
+			assert.equal(2, ratingCalculator.calculateRating(old_rating_4, old_rating_count_4, new_rating_4));
+		});
+	});
+});
